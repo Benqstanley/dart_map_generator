b73f85b0a693bab8eea4cb39c70f425bff1b717e
GitHub Action <action@github.com>
2025-08-28 15:43:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/benqstanley/dartmapgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/benqstanley/dartmapgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.benqstanley.dartmapgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/benqstanley/dartmapgenerator/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/benqstanley/dartmapgenerator/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.benqstanley.dartmapgenerator.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
